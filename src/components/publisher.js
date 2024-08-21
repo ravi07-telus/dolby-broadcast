ab4c5c0e1e38726990329a61828659a087a1de3a
ravi kumar <ravi.kumar07@telusinternational.com>
2024-08-21 18:28:15 +0530

    added viewer in dolby.io

--- src/components/publisher.js
+++ src/components/publisher.js
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import { Director, Publish } from '@millicast/sdk';
+import {useRef, useState} from 'react'
+import { Director, Publish, View } from '@millicast/sdk';
 
 const tokenGenerator = () => Director.getPublisher({
     token: '621bfca354d2ce378b99bd56cf754d8e1c9c51d6de90de3aa4d0d40b0d3a15d9', 
@@ -10,6 +10,8 @@ const publisher = new Publish('demo-stream', tokenGenerator);
 
 const  Publisher = ()=>{
     const [streamName, setStreamName] = useState('');
+    const [isStreaming,setIsStreaming]  = useState(false);
+    const videoRef = useRef(null)
 
     const startStream = async ()=>{
         const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
@@ -22,9 +24,32 @@ const  Publisher = ()=>{
         // Start publishing a stream
         try {
             await publisher.connect(publishOptions);
+            setIsStreaming(true);
+            // viewStream()
         } catch (e) {
             console.error('Connection failed, handle error', e);
         }
+    }
+
+
+    // const viewStream = ()=>{
+    //     // Create callback to generate a new token
+    //     const tokenGenerator = ()  => Director.getSubscriber({
+    //     streamName: 'demo-stream',
+    //     streamAccountId: 'vLasvj',
+    //     // Optional: This token is needed if you're subscribing to a secure stream,
+    //     // This token should be provided by the publish owner.
+    //         subscriberToken: '85681c968c5dc7bef7a131bb82185c24a84bae40bbfa7985b8bacaaff979a5c2',
+    //     });
+
+    //     // Create Millicast instance
+    //     const millicastView = new View('demo-stream', tokenGenerator,videoRef.current);
+    //     millicastView.connect();
+    // }
+
+    const stopStream = ()=>{
+        publisher.stop()
+        setIsStreaming(false);
     }
 
     return (
@@ -32,8 +57,13 @@ const  Publisher = ()=>{
         Broadcast
         <br />
         {/* <br />
-        <input name={'streamName'} value={streamName} placeholder='Enter Stream name' onChange={(e)=> setStreamName(e.target.value)}/> */}
+        <input name={'streamName'} value={streamNamewi} placeholder='Enter Stream name' onChange={(e)=> setStreamName(e.target.value)}/> */}
         <button onClick={startStream}> Start Streaming</button>
+        <button onClick={stopStream}> Stop Streaming</button>
+        {/* <video ref={videoRef} autoPlay controls style={{width:'50%'}}></video> */}
+        {isStreaming && <iframe src="https://viewer.millicast.com?streamId=vLasvj/demo-stream&muted=true&disableSettings=true" allowFullScreen width="640" height="480"></iframe>}
+        
+
     </div>
  )
 }
